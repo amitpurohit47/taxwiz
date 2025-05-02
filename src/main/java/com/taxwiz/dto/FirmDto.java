7034e6d1d2a757c66071dc284adac0790af21c26
Amit Purohit <amitpurohit4747@gmail.com>
2025-05-02 23:49:47 +0530

    Connected with Postgresql

---
+++ src/main/java/com/taxwiz/dto/FirmDto.java
@@ -0,0 +1,11 @@
+package com.taxwiz.dto;
+
+import lombok.Data;
+
+@Data
+public class FirmDto {
+    private String firmName;
+    private String gstNo;
+    private String email;
+    private String phone;
+}
--- src/main/java/com/taxwiz/service/firm/FirmAuthService.java
+++ src/main/java/com/taxwiz/service/firm/FirmAuthService.java
@@ -1,7 +1,9 @@
 package com.taxwiz.service.firm;
 
+import com.taxwiz.dto.FirmDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +11,7 @@ public class FirmAuthService {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    public void registerFirm() {
-
+    public void registerFirm(FirmDto firmRegisterRequest) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
     }
 }
