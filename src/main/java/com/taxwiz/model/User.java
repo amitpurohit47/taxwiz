c3ed1b62f0108804ccc39c3e0d19c548aa2f26d9
Amit Purohit <amitpurohit4747@gmail.com>
2025-05-06 04:39:50 +0530

    Root user setup

--- src/main/java/com/taxwiz/model/User.java
+++ src/main/java/com/taxwiz/model/User.java
@@ -1,4 +1,27 @@
 package com.taxwiz.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "taxwiz_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
 }
