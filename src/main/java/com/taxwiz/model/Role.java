599ba6d8a8da81ebdb642ef81d2891212f420e65
Amit Purohit <amitpurohit4747@gmail.com>
2025-05-06 20:00:04 +0530

    Root user setup

--- src/main/java/com/taxwiz/model/Role.java
+++ src/main/java/com/taxwiz/model/Role.java
@@ -1,4 +1,19 @@
 package com.taxwiz.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "taxwiz_roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
 }
